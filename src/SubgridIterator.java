6a807d9ebe14805315ee9ed599f8b7c235fa557c
Francis Radford <francis.radford@outlook.com>
2023-03-20 18:27:09 -0300

    Added sub grid iterator

---
+++ src/SubgridIterator.java
@@ -0,0 +1,63 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SubgridIterator implements Iterator<Cell> {
+    private SudokuBoard board;
+    private int curCol, minCol, maxCol, curRow, minRow, maxRow;
+    
+
+    public SubgridIterator(int subgrid, SudokuBoard board) {
+        this.board = board;
+
+        int cellSize = board.getCellSize();
+        this.minCol = cellSize * (subgrid % cellSize);
+        this.maxCol = this.minCol + cellSize;
+        this.minRow = cellSize * (subgrid / cellSize);
+        this.maxRow = this.minRow + cellSize - 1;
+        
+        this.curCol = this.minCol;
+        this.curRow = this.minRow;
+    }
+
+    /**
+     * Returns true if there are more values in the iterator
+     */
+    public boolean hasNext() {
+        return hasNextCol() || hasNextRow();
+    }
+
+    /**
+     * Returns the next cell if it exists
+     */
+    public Cell next() throws NoSuchElementException {
+        // check if we need to increment the row
+        if (hasNextRow() && !hasNextCol()) {
+            curRow++;
+            curCol = minCol;
+        }
+
+        // get the next cell
+        if (hasNextCol()) {
+            Cell next = board.getCell(curRow, curCol);
+
+            curCol++;
+            return next;
+        }
+
+        throw new NoSuchElementException();
+    }
+
+    private boolean hasNextCol() {
+        return this.curCol < this.maxCol;
+    }
+
+    private boolean hasNextRow() {
+        return this.curRow < this.maxRow;
+    }
+
+    public void reset () {
+        this.curCol = this.minCol;
+        this.curRow = this.minRow;
+    }
+
+}
