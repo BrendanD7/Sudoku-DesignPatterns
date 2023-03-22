02d48f8a8a7f5c60302daa24d39b699aa53f039a
Francis Radford <francis.radford@outlook.com>
2023-03-22 09:19:50 -0300

    update CellIterator

--- src/CellIterator.java
+++ src/CellIterator.java
@@ -1,6 +1,7 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public abstract class CellIterator implements Iterator<Cell> {
     public abstract boolean hasNext();
-    public abstract Cell next();
+    public abstract Cell next() throws NoSuchElementException;
 }
